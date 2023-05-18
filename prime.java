68e57f659bfeada5c44d9820d0973a7f676427aa
Himangshu <himangs503@gmail.com>
2023-05-18 08:29:02 +0530

    add prime number code

---
+++ prime.java
@@ -0,0 +1,28 @@
+public class prime {
+    public static void main(String[] args) {
+        int flag = 0;
+        int num1 = 1;
+        int num2 = 20;
+
+        // Printing all prime numbers between num1 to num2
+
+        for (int i = num1; i <= num2; i++)
+        {
+            flag = 0; // this variable is use to decide a number is prime or not
+            if (i == 1 || i == 2) {
+                System.out.println(i+" is a prime number");
+                continue; // Use to cancel current iteration
+            }
+            for (int j = 2; j <= i/2; j++) {
+                if (i%j == 0) {
+                    flag = 1;
+                    break; // use to break the inner loop
+                }
+            }          
+            if (flag == 0) {
+                System.out.println(i+" is a prime number");
+            }
+        }
+
+    }
+}
